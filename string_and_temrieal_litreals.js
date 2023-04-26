28a04cd5c81bff4ca01f6dda41e75dd98d45c827
Sunil9001934 <108231917+Sunil9001934@users.noreply.github.com>
2023-04-26 12:42:36 +0530

    Add files via upload

---
+++ string_and_temrieal_litreals.js
@@ -0,0 +1,45 @@
+const firstName = "sunil";
+const work = "Blackhathacking";
+const secondWork = "Machinelanguage applying many ways"
+const mynetwork = 5001
+const mynetworks = 5002
+const sunilNow = "I'a"+firstName+"my working not a working just a hobby"+ work +"my network is a " + mynetwork + "my second network is a "+ mynetworks+ "!";
+console.log(sunilNow);
+//////////////// Template literials
+const newwhoami = ` "i am" ${firstName}, working with group ${work}, my second work is a ${secondWork} mynetwork is ${mynetwork} mysecond network is a ${mynetworks} and i am a working in this field  `;
+console.log(newwhoami);
+console.log(`this is a regular string ......`
+)
+console.log(`this is a second string \n \ this is a second line
+\n\ this is a third line
+\n\ this is a fourth line  `);
+console.log(`this is a 
+nultiple line of sstring
+this is a third string in 
+this world 
+i am a hacker in this world 
+this is a javascript in this world 
+this is a fourth word in this word
+this is a fourth line in thsi world`)
+
+const age = 19
+const oldenugh = age >= 18;
+if (oldenugh) {
+    console.log(`this course for saraha saraha is a cool girl all this time my name is sunil`)
+}
+else {
+    const yearleft = age - 18;
+    console.log(`saraha is not enough this course he is very small in this time my name is sunil kumar kuri i ${yearleft} saraha is coming up this station pratice that every thing my name is sunil kumar and i am a softer hacker i know hacking like this magic trick but not just a magic trick just a very magical trick all ths timr`)
+}
+
+const birthYear = 2012;
+let centuary;
+if (birthYear <= 2000){
+    centuary = 20;}
+  else {
+    centuary = 21;
+ }
+ console.log(centuary)
+
+
+
